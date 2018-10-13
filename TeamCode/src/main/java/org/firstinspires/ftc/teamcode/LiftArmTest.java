53e64ee3940b1d207280598c5862e63fac96588a
sean Robertson <searob@icloud.com>
2018-10-13 15:28:12 -0700

    Added lift arm class, lift arm test, and hardware subsystem interface.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftArmTest.java
@@ -1,26 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
+@Autonomous
+public class LiftArmTest extends LinearOpMode {
+    LiftArm liftArm;
 
-@TeleOp(name="Lift Arm Test", group="iterative op mode")
-public class LiftArmTest extends BasicOpMode_Iterative {
-    private DcMotor top;
-    private DcMotor bottom;
-    @Override
-    public void init() {
-        top=hardwareMap.get(DcMotor.class, "top");
-        bottom=hardwareMap.get(DcMotor.class,"bottom");
-    }
-
-    @Override
-    public void loop() {
-        double power;
-        power = gamepad1.right_stick_y;
-        top.setPower(power);
-        bottom.setPower(power);
+    public void runOpMode(){
+        liftArm=new LiftArm(hardwareMap);
+        liftArm.init();
+        liftArm.raise();
     }
 }
